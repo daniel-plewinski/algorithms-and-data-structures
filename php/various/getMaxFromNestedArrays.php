0c0c3d8bfddcb07044802ed9a41bbdbe3f1231ab
Daniel Plewiński <d.plewinski@softlie.com.pl>
2019-03-12 08:06:28 +0100

    first commit

---
+++ php/various/getMaxFromNestedArrays.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = [10, [12, 40, [10, 999999], [[-100], 1000], [3, -9], 20]];
+$arr1 = [];
+foreach($arr as $key=>$value) {
+    if(!is_array($value)) {
+        $arr1[] = $value;
+    } else {
+        array_walk_recursive($value, function($val) use(&$arr1) {
+            $arr1[] = $val;
+        });
+    }
+}
+
+echo max($arr1);
