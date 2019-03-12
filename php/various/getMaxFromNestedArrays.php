1afacfbfc9beebe9aa68171029557044a972744d
Daniel Plewiński <d.plewinski@softlie.com.pl>
2019-03-12 12:08:08 +0100

    Array walk redundant element removed

--- php/various/getMaxFromNestedArrays.php
+++ php/various/getMaxFromNestedArrays.php
@@ -2,15 +2,10 @@
 
 $arr = [10, [12, 40, [10, 999999], [[-100], 1000], [3, -9], 20]];
 var_dump($arr);
+
 $arr1 = [];
-foreach($arr as $key=>$value) {
-    if(!is_array($value)) {
-        $arr1[] = $value;
-    } else {
-        array_walk_recursive($value, function($val) use(&$arr1) {
-            $arr1[] = $val;
-        });
-    }
-}
+array_walk_recursive($arr, function($val) use(&$arr1) {
+    $arr1[] = $val;
+});
 
 echo max($arr1);
