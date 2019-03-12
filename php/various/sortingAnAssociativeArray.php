8d5ec4724bf99954bb93fd8baa4eb628eacd8a94
Daniel Plewiński <d.plewinski@softlie.com.pl>
2019-03-12 10:31:03 +0100

    Associative array sorting modyfied

--- php/various/sortingAnAssociativeArray.php
+++ php/various/sortingAnAssociativeArray.php
@@ -24,13 +24,8 @@ var_dump($arr);
 usort($arr,function($item1, $item2) {
     
     $key = 'surname';
-    if ($item1[$key] == $item2[$key]) {
-        return false;
-    } elseif ($item1[$key] > $item2[$key]) {
-        return true;
-    } else {
-        return false;
-    }
+    if ($item1[$key] == $item2[$key]) return 0;
+    return ($item1[$key] < $item2[$key])?-1:1;
 });
 
 var_dump($arr);
